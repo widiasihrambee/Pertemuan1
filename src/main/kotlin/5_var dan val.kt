b1fc974b2a6ce9877346a21c5aa22e45adf2a8fc
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-11 16:21:01 +0700

    Percobaan val vs var

---
+++ src/main/kotlin/5_var dan val.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var varVariabel: Int
+    varVariabel = 56 //penetapan nilai pertama ke var variabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78  //penetapan nilai ke dua ke var variabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    val phi = 3.14
+    phi =2.5
+}
