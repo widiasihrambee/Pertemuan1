2470056c931253aa20c4fd30d4a69146bb1328bb
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-11 16:49:20 +0700

    Percobaan tipe data decimal

---
+++ src/main/kotlin/7_bilangan decimal.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //tipe data bilangan decimal (floating-point)
+
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+
+    println("maxFloat:"+ maxFloat)
+    println("maxDouble:"+ maxDouble)
+}
