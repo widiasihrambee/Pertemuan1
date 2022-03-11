2a004caaf62ebd050ff367e2c521827b69053988
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-11 16:49:49 +0700

    Percobaan tipe data literal konstan

---
+++ src/main/kotlin/8_literal konstan.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    //kode program, menggunakan literal konstan
+    val floatLiteral: Float=178.95f //literal float
+    println("contoh literal float:"+floatLiteral)
+    val oneBillion: Long = 1000000000 //literal Long
+    println("contoh literal long:"+ oneBillion)
+    val valHeksa= 0x0F //LITERAL HEKSADESIMAL DIAWALI DENGAN 0X
+    println("contoh literal heksadesimal:"+ valHeksa)
+    val valBinary = 0b00001011 //literal binary diawali dengan 0b
+    println("contoh litral binary:"+valBinary)
+    val notasiKonvensional = 123.5e10
+    println("contoh literal konvensional:"+notasiKonvensional)
+
+}
