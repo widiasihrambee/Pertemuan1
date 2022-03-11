05d1e5c285fd90914a302f8ef65ab8dba84d9f4d
RPL-NeliaDane <neliadane17@gmail.com>
2022-03-11 16:17:05 +0700

    Percobaan variabel

---
+++ src/main/kotlin/4_variabel.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //penggunan dynamicVar
+    var dynamicVar = 2
+    println("Contoh dynamic variabel =" + dynamicVar)
+    //penggunaan staticVar
+    var staticVar: Char = 'A'
+    println("Contoh static variabel =" + staticVar)
+
+
+
+
+}
