c4db7af584bb26575b31bce3b2aba7a1b5c3ad97
widiasihrambee <widiasihrambe@gmail.com>
2022-03-17 21:55:38 +0700

    Percobaan program kalkulator sederhana

---
+++ src/main/kotlin/program kalkulatur sederhana.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // simple calculator
+    println("welcome to calculator")
+    println("Enter the first number h:")
+    val a = readLine()!!.toDouble()
+    println("Enter the second number:")
+    val b = readLine()!!.toDouble()
+    val sum = a+ b
+    val difference = a- b
+    val product = a * b
+    val quotient = a / b
+    println("Sum : ${sum}")
+    println("Difference: ${difference}")
+    println("Product: ${product }")
+    println("Quotient: ${quotient}")
+    println("Thank you for using alculator.")
+}
