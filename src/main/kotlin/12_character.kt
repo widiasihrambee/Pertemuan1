58fc557539a826b4175381f5f6473c8f7ab18a8f
widiasihrambee <widiasihrambe@gmail.com>
2022-03-17 17:26:06 +0700

    Percobaan character

---
+++ src/main/kotlin/12_character.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var charA : Char= 'A'
+    //println(charA == 65)
+
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String= charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
+}
