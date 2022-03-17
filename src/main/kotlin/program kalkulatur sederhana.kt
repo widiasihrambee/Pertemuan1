f9206e525efc0d40c82f7be8cf7973e54d728d30
widiasihrambee <widiasihrambe@gmail.com>
2022-03-17 21:59:29 +0700

    Percobaan programm kalkulator sederhana

--- src/main/kotlin/program kalkulatur sederhana.kt
+++ src/main/kotlin/program kalkulatur sederhana.kt
@@ -13,5 +13,5 @@ fun main() {
     println("Difference: ${difference}")
     println("Product: ${product }")
     println("Quotient: ${quotient}")
-    println("Thank you for using alculator.")
+    println("Thank you for using calculator.")
 }
